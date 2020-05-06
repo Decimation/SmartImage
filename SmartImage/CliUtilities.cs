794ca904fae20b8209998212804de53fe58434de
Read <decimation001@gmail.com>
2020-05-06 02:10:30 -0500

    clp progress 2

--- SmartImage/CliUtilities.cs
+++ SmartImage/CliUtilities.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using CommandLine;
+using SmartImage.Engines.SauceNao;
+using static SmartImage.CmdFunctions;
 
 namespace SmartImage
 {
@@ -20,7 +22,7 @@ namespace SmartImage
 			 * Verbs 
 			 */
 
-			var types = CmdFunctions.LoadVerbs();
+			var types = LoadVerbs();
 			types.Add(typeof(ArgConfig));
 
 
@@ -28,7 +30,7 @@ namespace SmartImage
 			      .WithParsed(o =>
 			       {
 				       Console.WriteLine("parse: {0}", o);
-				       var obj = CmdFunctions.Run(o, args);
+				       var obj = Run(o, args);
 			       })
 			      .WithNotParsed(CliUtilities.HandleErrors);
 		}
@@ -40,16 +42,12 @@ namespace SmartImage
 			 */
 
 			//
-			ArgConfig cfg = null;
+			ArgConfig cfg = new ArgConfig();
 
 
-			var r1 = Parser.Default.ParseArguments<ArgConfig>(args);
-			r1.WithParsed(p =>
-			{
-				
-				cfg = p;
-			});
-			if (cfg!=null) {
+			/*var r1 = Parser.Default.ParseArguments<ArgConfig>(args);
+			r1.WithParsed(p => { cfg = p; });
+			if (cfg != null) {
 				if (cfg.__simple) {
 					Console.WriteLine("using cfg file fallback");
 
@@ -58,13 +56,56 @@ namespace SmartImage
 						Console.WriteLine("????");
 					}
 
-					ArgConfig.ReadFromFile(cfg,AltConfig.ConfigLocation);
+					ArgConfig.ReadFromFile(cfg, AltConfig.ConfigLocation);
+				}
+			}*/
+			var r1 = Parser.Default.ParseArguments<ArgConfig, ContextMenu, Path,
+				CreateSauceNao, Reset, Info>(args);
+
+			r1.WithParsed<ArgConfig>(p =>
+			{
+				cfg = p;
+
+				
+			});
+			
+			
+			
+			if (cfg != null) {
+				if (cfg.__simple) {
+					Console.WriteLine("using cfg file fallback");
+
+					bool imgOnly = args.Length == 1;
+					if (!imgOnly) {
+						Console.WriteLine("????");
+					}
+
+					ArgConfig.ReadFromFile(cfg, AltConfig.ConfigLocation);
 				}
 			}
 
-			
-			
-			
+			r1.WithParsed<ContextMenu>(c =>
+			{
+				if (c.Add) {
+					ContextMenu.AddToContextMenu();
+				}
+				else if (c.Remove) {
+					ContextMenu.RemoveFromContextMenu();
+				}
+			});
+			r1.WithParsed<Path>(c =>
+			{
+				if (c.Add) {
+					Path.AddToPath();
+				}
+				else {
+					Path.RemoveFromPath();
+				}
+			});
+			r1.WithParsed<CreateSauceNao>(c => { SauceNao.CreateAccount(c.Auto); });
+			r1.WithParsed<Reset>(c => { Reset.RunReset(c.All); });
+			r1.WithParsed<Info>(c => { Info.ShowInfo(c.Full); });
+
 			//ReadFuncs(args);
 
 			return cfg;
