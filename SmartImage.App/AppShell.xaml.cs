60da755f29e060dbfdd3bf73ddadd78b8f24e889
Read <decimation001@gmail.com>
2022-12-11 15:35:56 -0600

    Start work on MAUI

---
+++ SmartImage.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartImage.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartImage.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartImage.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
