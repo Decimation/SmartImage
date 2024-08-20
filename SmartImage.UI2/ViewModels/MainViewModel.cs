449c8c1694b75394bebf196be7b9facab186c857
Read Stanton <decimation001@gmail.com>
2024-08-20 01:15:57 -0500

    *

--- SmartImage.UI2/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SmartImage.UI2.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- SmartImage.UI2/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace SmartImage.UI2.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
