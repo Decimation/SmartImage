8deaf74d8e769dfb5c656ad8235977ad973f5601
Read Stanton <decimation001@gmail.com>
2026-02-21 18:35:17 -0600

    untracked files on (no branch): a79d100 ASCENSION 999 (stash)

---
+++ SmartImage.Lib/Model/ISize.cs
@@ -0,0 +1,21 @@
+﻿// Author: Deci | Project: SmartImage.Lib | Name: ISize.cs
+// Date: 2026/02/21 @ 18:02:14
+
+namespace SmartImage.Lib.Model;
+
+public interface ISize
+{
+
+	/// <summary>
+	///     Image width
+	/// </summary>
+	int? Width { get; set; }
+
+	/// <summary>
+	///     Image height
+	/// </summary>
+	int? Height { get; set; }
+
+	bool HasDimensions { get; }
+
+}
